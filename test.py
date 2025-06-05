af1377983cef93db52b3d51dba273ccbaaf573ec
Sascha Cowley <16543535+SaschaCowley@users.noreply.github.com>
2025-06-05 15:47:21 +1000

    Add tests for server state

--- test.py
+++ test.py
@@ -10,3 +10,38 @@ class TestUser(unittest.TestCase):
 		"""Test that creating several users sequentially creates them with sequential user IDs."""
 		users = (User(mock.Mock(Handler)) for _ in range(10))
 		self.assertSequenceEqual(list(map(lambda user: user.user_id, users)), range(1, 11))
+
+
+class TestServerState(unittest.TestCase):
+	def setUp(self) -> None:
+		self.serverState = ServerState()
+
+	def _addChannels(self) -> list[Channel]:
+		channels = [
+			Channel(key, self.serverState)
+			for key in 'abcd'
+		]
+		self.serverState.channels.update({channel.key: channel for channel in channels})
+		return channels
+	
+	def test_findOrCreateChannel_create(self):
+		"""Check that passing a new key creates a new channel."""
+		extantChannels = self._addChannels()
+		oldChannels = self.serverState.channels.copy()
+		self.assertNotIn("newChannel", self.serverState.channels)
+		newChannel = self.serverState.find_or_create_channel("newChannel")
+		self.assertIn("newChannel", self.serverState.channels)
+		self.assertIs(self.serverState.channels["newChannel"], newChannel)
+		self.assertNotIn(newChannel, extantChannels)
+		self.assertNotEqual(oldChannels, self.serverState.channels)
+	
+	def test_findOrCreateChannel_find(self):
+		"""Check that passing an existant key returns the associated channelcreates a new channel."""
+		exttantChannels = self._addChannels()
+		oldChannels = self.serverState.channels.copy()
+		self.assertIn('c', self.serverState.channels)
+		expectedChannel = exttantChannels[2]
+		foundChannel = self.serverState.find_or_create_channel('c')
+		self.assertIs(expectedChannel, foundChannel)
+		self.assertEqual(oldChannels, self.serverState.channels)
+
